ceb6b32a1d0179469ad5ac3fa49b43e67c61676f
junsu0302 <junsu96378224@gmail.com>
2025-03-20 23:47:29 +0900

    #2 - Pipeline 6 : Get KR Adjusted Price
    한국 주식 중 지정한 주식의 수정 주가를 반환한다.

---
+++ data/pipeline/get_kr_adjusted_price.py
@@ -0,0 +1,16 @@
+import pandas as pd
+from pipeline.kr_stock.transform_krx_adjusted_price import transform_krx_adjusted_price
+
+
+def get_kr_adjusted_price(cd: str, isin: str, nm: str) -> pd.DataFrame:
+  """KRX에서 수집한 특정 종목의 수정 주가 데이터를 사용하기 좋은 형태로 변환하여 반환하는 함수
+
+  Args:
+    cd (str): 종목 코드
+    isin (str): 표준 코드 (ISIN)
+    nm (str): 종목 명
+
+  Returns:
+    pd.DataFrame: 변환된 수정 주가 데이터 (['cmp_cd', 'trade_date', 'close_price', 'price_diff', 'fluctuation_rt', 'open_price', 'high_price', 'low_price', 'trade_volume', 'trade_value', 'market_cap', 'listed_shares'])
+  """
+  return transform_krx_adjusted_price(cd, isin, nm)
