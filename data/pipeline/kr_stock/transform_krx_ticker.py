087ee8e4669f6c70a83457c9df6219ed95bd008e
junsu0302 <junsu96378224@gmail.com>
2025-03-21 15:02:07 +0900

    #5 - Ticker Pipeline 2 : Transform KRX Ticker
    명칭을 sector에서 ticker로 변경하고, 시가 총액의 경우 kr_stock_price에서 확인 가능하므로 제거한다.

--- data/pipeline/kr_stock/transform_krx_ticker.py
+++ data/pipeline/kr_stock/transform_krx_ticker.py
@@ -1,15 +1,15 @@
 import pandas as pd
-from .fetch_krx_sector import fetch_krx_sector
+from .fetch_krx_ticker import fetch_krx_ticker
 from utils.state_print import state_print
 
-def add_gics_column(krx_sector:pd.DataFrame) -> pd.DataFrame:
+def add_gics_column(krx_ticker:pd.DataFrame) -> pd.DataFrame:
   """KRX 업종명을 바탕으로 GICS 코드 생성
 
   Args:
-    krx_sector (pd.DataFrame): GICS 코드를 추가할 한국 주식 섹터 정보 
+    krx_ticker (pd.DataFrame): GICS 코드를 추가할 한국 주식 섹터 정보 
 
   Returns:
-    pd.DataFrame: GICS 코드가 추가된 한국 주식 섹터 정보
+    pd.DataFrame: [cmp_cd, cmp_nm, mkt_type, gics_cd, mkt_cap_rt, ref_dt]
   """
   # KRX 업종명을 GICS 기준으로 매핑
   gics_mapping = {
@@ -41,65 +41,64 @@ def add_gics_column(krx_sector:pd.DataFrame) -> pd.DataFrame:
     return "N/A"
 
   # KRX 섹터 데이터에 GICS 컬럼 추가 (업종명 기반 매핑)
-  krx_sector[["gics_code"]] = krx_sector["업종명"].apply(lambda x: pd.Series(get_gics_info(str(x))))
+  krx_ticker[["gics_code"]] = krx_ticker["업종명"].apply(lambda x: pd.Series(get_gics_info(str(x))))
 
   state_print("WHITE", "- GICS Code 생성 완료")
 
-  return krx_sector
+  return krx_ticker
 
-def add_gics_rate_column(krx_sector:pd.DataFrame) -> pd.DataFrame:
+def add_gics_rate_column(krx_ticker:pd.DataFrame) -> pd.DataFrame:
   """GICS 섹터에서 현재 주식의 총 비율을 반환하는 함수
 
   Args:
-    krx_sector (pd.DataFrame): GICS 섹터가 존재하는 주식 데이터
+    krx_ticker (pd.DataFrame): GICS 섹터가 존재하는 주식 데이터
 
   Returns:
     pd.DataFrame: GICS 비율이 포함된 주식 데이터
   """
   # gics_code별 시가총액 총합 계산
-  krx_sector['시가총액'] = pd.to_numeric(krx_sector['시가총액'], errors='coerce')
-  total_market_cap_by_gics = krx_sector.groupby('gics_code')['시가총액'].sum().reset_index()
+  krx_ticker['시가총액'] = pd.to_numeric(krx_ticker['시가총액'], errors='coerce')
+  total_market_cap_by_gics = krx_ticker.groupby('gics_code')['시가총액'].sum().reset_index()
   total_market_cap_by_gics = total_market_cap_by_gics.rename(columns={'시가총액': 'gics_total_market_cap'})
 
   # 원래 데이터프레임과 병합
-  krx_sector = krx_sector.merge(total_market_cap_by_gics, on='gics_code', how='left')
+  krx_ticker = krx_ticker.merge(total_market_cap_by_gics, on='gics_code', how='left')
 
   # 각 종목의 gics_code 내 시가총액 비율 계산
-  krx_sector['시가총액_비율'] = (krx_sector['시가총액'] / krx_sector['gics_total_market_cap']) * 100
+  krx_ticker['시가총액_비율'] = (krx_ticker['시가총액'] / krx_ticker['gics_total_market_cap']) * 100
 
   state_print("WHITE", "- GICS Code Rate 생성 완료")
 
-  return krx_sector
+  return krx_ticker
 
-def rename_columns(krx_sector:pd.DataFrame) -> pd.DataFrame:
+def rename_columns(krx_ticker:pd.DataFrame) -> pd.DataFrame:
   """한국 주식의 이름을 변경
 
   Args:
-    krx_sector (pd.DataFrame): 이름을 변경할 한국 주식 데이터
+    krx_ticker (pd.DataFrame): 이름을 변경할 한국 주식 데이터
 
   Returns:
     pd.DataFrame: 이름이 변경된 한국 주식 데이터
   """
-  krx_sector = krx_sector[['종목코드', '종목명', '시가총액', '시장구분', 'gics_code', '시가총액_비율', '기준일']]
-  krx_sector = krx_sector.rename(columns={
-    '종목코드': 'CMP_CD', #기업 코드
-    '종목명': 'CMP_NM', # 기업 이름
-    '시가총액': 'MKT_CAP',  # 시가총액
-    '시장구분': 'MKT_TYPE', # 시장 구분
-    'gics_code': 'GICS_CD', # GICS(산업분류) 코드
-    '시가총액_비율': 'MKT_CAP_RT',  # GICS 내 시가총액 비율
-    '기준일': 'REF_DT'  # 데이터 기준일 
+  krx_ticker = krx_ticker[['종목코드', '종목명', '시장구분', 'gics_code', '시가총액_비율', '기준일']]
+  krx_ticker = krx_ticker.rename(columns={
+    '종목코드': 'cmp_cd', #기업 코드
+    '종목명': 'cmp_nm', # 기업 이름
+    '시장구분': 'mkt_type', # 시장 구분
+    'gics_code': 'gics_cd', # GICS(산업분류) 코드
+    '시가총액_비율': 'mkt_cap_rt',  # GICS 내 시가총액 비율
+    '기준일': 'ref_dt'  # 데이터 기준일 
   })
 
   state_print("WHITE", "- 주식 데이터 컬럼 명 변경 완료")
 
-  return krx_sector
+  return krx_ticker
 
-def transform_krx_sector():
+def transform_krx_ticker():
   """KOSPI/KOSDAQ 업종 데이터 변환"""
-  krx_sector = fetch_krx_sector()
-  krx_sector = add_gics_column(krx_sector)
-  krx_sector = add_gics_rate_column(krx_sector)
-  krx_sector = rename_columns(krx_sector)
+  krx_ticker = fetch_krx_ticker()
+  krx_ticker = add_gics_column(krx_ticker)
+  krx_ticker = add_gics_rate_column(krx_ticker)
+  krx_ticker = rename_columns(krx_ticker)
   state_print("GREEN", "✅ KOSPI/KOSDAQ 업종 데이터 변환 완료")
-  return krx_sector
+  return krx_ticker
